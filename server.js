03a4bf8eedcb2454ddca06c4dd6f72af0d9ef880
Michelle Pinar <michelle-pinar@Michelles-MacBook-Air.local>
2022-11-12 23:41:41 -0500

    added all new files to project

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const fs = require("fs");
+// const hostname = "127.0.0.1";
+const port = 8000;
+var newJsonDataToAppend;
+
+var app = express();
+
+app.listen(port, () => {
+  console.log("My project app listening on port 8000!");
+});
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/", (request, response) => {
+  newJsonDataToAppend = request;
+  fs.appendFile("data.json", newJsonDataToAppend);
+});
